f999e54e893c5dfe974e25018a053afa6c309c88
siyabonga <siyabonga.hlengwa@mirumagency.com>
2020-07-03 22:21:43 +0200

    adding projects section

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-$(document).ready(function(){
-  $("#aboutMe").click(function(){
+$(document).ready(function () {
+  $("#aboutMe").click(function () {
     $("#about").fadeIn(1000);
     $("#edEx").hide();
     $("#skills").hide();
+    $("#work").hide();
   });
-   $("#Educ-Expe").click(function(){
+  $("#Educ-Expe").click(function () {
     $("#about").hide();
     $("#edEx").fadeIn(1000);
     $("#skills").hide();
+    $("#work").hide();
   });
-$("#Skills").click(function(){
+  $("#Skills").click(function () {
     $("#about").hide();
     $("#edEx").hide();
     $("#skills").fadeIn(1000);
+    $("#work").hide();
+  });
+  $("#Projects").click(function () {
+    $("#about").hide();
+    $("#edEx").hide();
+    $("#skills").hide();
+    $("#work").fadeIn(1000);
+
   });
 });
