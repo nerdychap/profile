2eb0a0eb68c4ffb68d8e63d53755382049ddcc83
Siyabonga Hlengwa <44382048+nerdychap@users.noreply.github.com>
2023-07-27 20:45:44 +0000

    modifying the portfolio with updated information and styling

--- script.js
+++ script.js
@@ -4,24 +4,31 @@ $(document).ready(function () {
     $("#edEx").hide();
     $("#skills").hide();
     $("#work").hide();
+    $("#sideBox").hide();
+
   });
   $("#Educ-Expe").click(function () {
     $("#about").hide();
     $("#edEx").fadeIn(1000);
     $("#skills").hide();
     $("#work").hide();
+    $("#sideBox").hide();
+
   });
   $("#Skills").click(function () {
     $("#about").hide();
     $("#edEx").hide();
     $("#skills").fadeIn(1000);
     $("#work").hide();
+    $("#sideBox").hide();
   });
   $("#Projects").click(function () {
     $("#about").hide();
     $("#edEx").hide();
     $("#skills").hide();
     $("#work").fadeIn(1000);
+    $("#sideBox").fadeIn(1000);
+
 
   });
 });
